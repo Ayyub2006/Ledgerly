a5113d986a47e682d39a921cfe93834a1cb500f1
Ayyub2006 <ayyubkhan0607@gmail.com>
2026-07-07 15:19:05 +0530

    Initial Spring Boot project setup

---
+++ src/test/java/com/ayyub/PersonalExpenseTracker/PersonalExpenseTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayyub.PersonalExpenseTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalExpenseTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
