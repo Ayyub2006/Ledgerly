c3bcce08e2ec84330f5b753fa6b93484af06444f
Ayyub2006 <ayyubkhan0607@gmail.com>
2026-07-15 09:26:49 +0530

    feat: integrate Swagger OpenAPI documentation with JWT authentication

--- src/main/java/com/ayyub/PersonalExpenseTracker/config/SecurityConfig.java
+++ src/main/java/com/ayyub/PersonalExpenseTracker/config/SecurityConfig.java
@@ -29,7 +29,12 @@ public class SecurityConfig {
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
 		http.csrf(csrf -> csrf
 				.disable())
-		.authorizeHttpRequests(auth -> auth.requestMatchers("/api/auth/**")
+		.authorizeHttpRequests(auth -> auth.requestMatchers(
+		        "/api/auth/**",
+		        "/v3/api-docs/**",
+		        "/swagger-ui/**",
+		        "/swagger-ui.html"
+		)
 		.permitAll()
 		.anyRequest()
 		.authenticated())
